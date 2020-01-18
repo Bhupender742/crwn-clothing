0048ecd7b0d04e10c688b08d4e8680f8d937a278
Bhupender Rawat <bhupenderrawat742@gmail.com>
2020-01-18 13:50:55 +0530

    converted our CartItem, CheckoutItem and CollectionItem components to use styled-components instead of scss

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import './checkout-item.styles.scss';
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    TextContainer,
+    QuantityContainer,
+    RemoveButtonContainer
+} from './checkout-item.styles';
 
 import { clearItemFromCart, addItem, decreaseItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem , clearItem, addItem, decreaseItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, decreaseItem }) => {
     const { imageUrl, name, quantity, price } = cartItem;
     return (
-        <div className='checkout-item'>
-            <div className='image-container'>
-                <img src={imageUrl} alt='item'/>
-            </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>
-                <div className='arrow' onClick={() => decreaseItem(cartItem)}>&#10094;</div>
-                <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
-            </span>
-            <span className='price'>{price}</span>
-            <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
-        </div>
-)}
+        <CheckoutItemContainer>
+            <ImageContainer>
+                <img src={imageUrl} alt='item' />
+            </ImageContainer>
+            <TextContainer>{name}</TextContainer>
+            <QuantityContainer>
+                <div onClick={() => decreaseItem(cartItem)}>&#10094;</div>
+                <span>{quantity}</span>
+                <div onClick={() => addItem(cartItem)}>&#10095;</div>
+            </QuantityContainer>
+            <TextContainer>{price}</TextContainer>
+            <RemoveButtonContainer onClick={() => clearItem(cartItem)}>
+                &#10005;
+            </RemoveButtonContainer>
+        </CheckoutItemContainer>
+    )
+}
 
 const mapDispatchToProps = dispatch => ({
     clearItem: item => dispatch(clearItemFromCart(item)),
